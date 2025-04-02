e8ffd14f8145a9fead2c45e445c93439b0f6083f
Debangshu Roy <143197283+myselfdeb@users.noreply.github.com>
2025-04-02 23:18:10 +0530

    Add files via upload

---
+++ 1_Day.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main() {
+    int n;
+    printf("Enter the side of the square : \n");
+    scanf("%d",&n);
+    printf("The area of the square is :%d\n",n*n);
+    return 0;
+}
